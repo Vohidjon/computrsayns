8d85371c87f8397368756ef6916cc44e5a7d2a95
Vohidjon <abduvohid.karimjonov@gmail.com>
2018-08-13 12:36:19 +0500

    dynamic programming

--- src/dynamicprogramming/JobSchedulingProblem.java
+++ src/dynamicprogramming/JobSchedulingProblem.java
@@ -2,6 +2,38 @@ package dynamicprogramming;
 
 public class JobSchedulingProblem {
     public int maxProfit(int[] starts, int[] ends, int[] profits) {
+        int[] table = buildTable(starts, ends, profits);
+
+        int max = table[0];
+        for (int i = 0; i < table.length; i++) {
+            if(max < table[i]) max = table[i];
+        }
+        return max;
+    }
+
+    public String pickJobs(int[] starts, int[] ends, int[] profits) {
+        int[] table = buildTable(starts, ends, profits);
+
+        int maxIndex = 0;
+        for (int i = 0; i < table.length; i++) {
+            if(table[maxIndex] < table[i]) maxIndex = i;
+        }
+        return reconstruct(table, starts, ends, profits, maxIndex);
+    }
+
+    private String reconstruct(int[] costs, int[] starts, int[] ends,  int[] profits, int i) {
+        if(costs[i] - profits[i] == 0) return "[" + starts[i] + "," + ends[i] + "]";
+        else {
+            int leftCost = costs[i] - profits[i];
+            int j = i;
+            while (true) {
+                j--;
+                if(costs[j] == leftCost) return reconstruct(costs, starts, ends, profits, j) + "[" + starts[i] + "," + ends[i] + "]";
+            }
+        }
+    }
+
+    private int[] buildTable(int[] starts, int[] ends, int[] profits) {
         int[] costs = new int[starts.length];
         for (int i = 0; i < profits.length; i++) {
             costs[i] = profits[i];
@@ -9,16 +41,11 @@ public class JobSchedulingProblem {
 
         for (int i = 0; i < starts.length; i++) {
             for (int j = 0; j < i; j++) {
-                if(ends[j] <= ends[i]) {
+                if(ends[j] <= starts[i]) {
                     costs[i] = Math.max(costs[i], costs[j] + profits[i]);
                 }
             }
         }
-
-        int max = costs[0];
-        for (int i = 0; i < costs.length; i++) {
-            if(max < costs[i]) max = costs[i];
-        }
-        return max;
+        return costs;
     }
 }
--- src/dynamicprogramming/Test.java
+++ src/dynamicprogramming/Test.java
@@ -2,6 +2,9 @@ package dynamicprogramming;
 
 public class Test {
     public static void main(String[] args) {
-
+        System.out.println(new JobSchedulingProblem().pickJobs(
+                new int[]{1, 2, 4, 6, 5, 7},
+                new int[]{3, 5, 6, 7, 8, 9},
+                new int[]{5, 6, 5, 4, 11, 2}));
     }
 }
