ee6d009f7481f63773cd12a00c27e94008a6dae8
Vohidjon <abduvohid.karimjonov@gmail.com>
2018-10-19 01:04:32 +0500

    leet code

--- src/leetcode/medium/BinaryTreeLevelOrderTraversal.java
+++ src/leetcode/medium/BinaryTreeLevelOrderTraversal.java
@@ -1,43 +1,40 @@
 package leetcode.medium;
 
-import jdk.nashorn.api.tree.Tree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
+import leetcode.TreeNode;
 
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<TreeNode> list = new ArrayList<>();
-        list.add(root);
-        list.add(null);
         List<List<Integer>> result = new ArrayList<>();
+        LinkedList<NodeWrapper> queue = new LinkedList<>();
+        queue.add(new NodeWrapper(root));
+        queue.add(new NodeWrapper());
         List<Integer> level = new ArrayList<>();
-        int i = 0;
-        while (i < list.size()) {
-            TreeNode elem = list.get(i);
-            if (elem == null) {
-                if (level.size() != 0) result.add(level);
-                list.add(null);
+        while (!queue.isEmpty()) {
+            NodeWrapper nodeWrapper = queue.poll();
+            if (nodeWrapper.node == null && !level.isEmpty()) {
+                result.add(level);
                 level = new ArrayList<>();
-            } else {
-                level.add(elem.val);
-                if (elem.left != null) list.add(elem.left);
-                if (elem.right != null) list.add(elem.right);
+                queue.add(new NodeWrapper());
+            } else if (nodeWrapper.node != null) {
+                level.add(nodeWrapper.node.val);
+                if(nodeWrapper.node.left != null) queue.add(new NodeWrapper(nodeWrapper.node.left));
+                if(nodeWrapper.node.right != null) queue.add(new NodeWrapper(nodeWrapper.node.right));
             }
-            i++;
         }
         return result;
+    }
+
+    class NodeWrapper {
+        TreeNode node;
+
+        NodeWrapper() {
+        }
+
+        NodeWrapper(TreeNode treeNode) {
+            node = treeNode;
+        }
     }
 }
